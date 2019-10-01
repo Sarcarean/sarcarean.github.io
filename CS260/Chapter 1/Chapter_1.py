8b7a3ea8d7edc59cec0649da6ff1ae4b9f47fe47
rharris7@cocc.edu <rharris7@cocc.edu>
2019-10-01 14:56:34 -0700

    updated

--- CS260/Chapter 1/Chapter_1.py
+++ CS260/Chapter 1/Chapter_1.py
@@ -1,1 +1,22 @@
+import unittest
+
+import Math_Fract
+
+
+class TestFractionModule(unittest.TestCase):
+
+    def test_addition(self):
+        f1 = Math_Fract.Fraction(1,4)
+        f2 = Math_Fract.Fraction(1,2)
+        f3 = f1+f2
+        self.assertEqual(f3, Math_Fract.Fraction(3,4))
+
+    def test_str(self):
+        f1 = Math_Fract.Fraction(1,2)
+        self.assertEqual(str(f1), '1/2')
+
+if __name__ == '__main__':
+    #raise ValueError
+    unittest.main(exit=False)
+
 
