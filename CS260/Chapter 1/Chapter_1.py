05b23751433eb21d9adba2232144adbb89b0e02d
R. Harris <43601394+Sarcarean@users.noreply.github.com>
2019-10-02 01:47:58 -0700

    Finished Chapter 1

--- CS260/Chapter 1/Chapter_1.py
+++ CS260/Chapter 1/Chapter_1.py
@@ -1,22 +1,58 @@
 import unittest
-
 import Math_Fract
 
-
 class TestFractionModule(unittest.TestCase):
-
+    """These are all of the unit tests that use the Fraction class"""
     def test_addition(self):
         f1 = Math_Fract.Fraction(1,4)
         f2 = Math_Fract.Fraction(1,2)
         f3 = f1+f2
         self.assertEqual(f3, Math_Fract.Fraction(3,4))
 
+    def test_subtraction(self):
+        f1 = Math_Fract.Fraction(3,8)
+        f2 = Math_Fract.Fraction(1,4)
+        f3 = f1-f2
+        self.assertEqual(f3, Math_Fract.Fraction(1,8))
+
+    def test_multiplication(self):
+        f1 = Math_Fract.Fraction(1,2)
+        f2 = Math_Fract.Fraction(4,5)
+        f3 = f1*f2
+        self.assertEqual(f3, Math_Fract.Fraction(2,5))
+
+    def test_division(self):
+        f1 = Math_Fract.Fraction(1,2)
+        f2 = Math_Fract.Fraction(4,5)
+        f3 = f1/f2
+        self.assertEqual(f3, Math_Fract.Fraction(5,8))
+
     def test_str(self):
         f1 = Math_Fract.Fraction(1,2)
         self.assertEqual(str(f1), '1/2')
 
+    def test_equal(self):
+        f1 = Math_Fract.Fraction(1,2)
+        f2 = Math_Fract.Fraction(1,2)
+        self.assertEqual(f1==f2, True)
+
+    def test_not_equal(self):
+        f1 = Math_Fract.Fraction(1,2)
+        f2 = Math_Fract.Fraction(3,2)
+        self.assertEqual(f1!=f2, True)
+
+    def test_less_than(self):
+        f1 = Math_Fract.Fraction(1,8)
+        f2 = Math_Fract.Fraction(1,4)
+        self.assertEqual(f1<f2, True)
+
+    def test_greater_than(self):
+        f1 = Math_Fract.Fraction(7,8)
+        f2 = Math_Fract.Fraction(1,4)
+        self.assertEqual(f1>f2, True)
+
+
 if __name__ == '__main__':
-    #raise ValueError
     unittest.main(exit=False)
 
 
--- CS260/Chapter 1/Math_Fract.py
+++ CS260/Chapter 1/Math_Fract.py
@@ -7,29 +7,88 @@ def gcd(m,n):
     return n
 
 class Fraction:
-
-    def __init__(self,top,bottom):
-        self.num = top
-        self.den = bottom
+    """A class that creates a fraction object"""
+    def __init__(self, top, bottom):
+        """The fraction class constructor"""
+        if not type(top) != type(int):
+            raise Exception("Numerator is not an integer")
+        if not type(bottom) != type(int):
+            raise Exception("Denominator is not an integer")
+        if bottom<0:
+            bottom = bottom*-1
+            top = abs(top)*-1
+        common = gcd(top,bottom)
+        self.num = top//common
+        self.den = bottom//common
 
     def show(self):
+        """The fraction class constructor"""
         print(self.num,"/",self.den)
 
     def __str__(self):
+        """The fraction class constructor"""
         return str(self.num)+"/"+str(self.den)
 
-    def __add__(self,otherfraction):
+    def __repr__(self):
+        """The fraction class constructor"""
+        return self.__str__
+
+    def __add__(self, otherfraction):
+        """Performs a fraction addition operation, returns a fraction"""
         newnum = self.num*otherfraction.den + self.den*otherfraction.num
         newden = self.den * otherfraction.den
         return Fraction(newnum,newden)
 
-    def __add__(self,otherfraction):
-        newnum = self.num*otherfraction.den + self.den*otherfraction.num
+    def __radd__(self, otherfraction):
+        """Performs a fraction addition operation, returns a fraction"""
+        return self.__add__(otherfraction)
+
+    def __iadd__(self, otherfraction):
+        """Performs a fraction addition operation, returns a fraction"""
+        return self.__add__(otherfraction)
+
+    def __sub__(self, otherfraction):
+        """Performs a fraction subtraction, returns a fraction"""
+        newnum = self.num*otherfraction.den - self.den*otherfraction.num
         newden = self.den * otherfraction.den
-        common = gcd(newnum,newden)
-        return Fraction(newnum//common,newden//common)
+        return Fraction(newnum,newden)
+
+    def __mul__(self, otherfraction):
+        """Performs a fraction mulitplication, returns a fraction"""
+        newnum = self.num*otherfraction.num
+        newden = self.den*otherfraction.den
+        return Fraction(newnum,newden)
+
+    def __truediv__(self, otherfraction):
+        """Performs a fraction division, returns a fraction"""
+        newnum = self.num*otherfraction.den
+        newden = self.den*otherfraction.num
+        return Fraction(newnum,newden)
 
     def __eq__(self, other):
+        """Returns bool result of equals"""
         firstnum = self.num * other.den
         secondnum = other.num * self.den
         return firstnum == secondnum
+
+    def __ne__(self, other):
+        """Returns bool result of not equals"""
+        return not self.__eq__(other)
+
+    def __lt__(self, other):
+        """Returns bool result of less than"""
+        first = self.num/self.den
+        second = other.num/other.den
+        return first<second
+
+    def __gt__(self, other):
+        """Returns bool result of greater than"""
+        return not self.__lt__(other)
+
+    def __le__(self, other):
+        """Returns bool result of less than or equal"""
+        return self.__lt__(other) or self.__eq__(other)
+
+    def __ge__(self, other):
+        """Returns bool result of greater than or equal"""
+        return self.__gt__(other) or self.__eq__(other)
